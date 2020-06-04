bf7b7d8e4e061bfe788c432e58f947fb47e57880
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2020-06-04 13:51:19 +0700

    Update tester

--- test_src/tester.c
+++ test_src/tester.c
@@ -6,7 +6,7 @@ int main()
     uint32_t len;
     // Read the message
     uint8_t* msg = NULL;
-    msg = nativemsg_read_message(&len);
+    msg = nativemsg_read(&len);
     if (msg == NULL)
     {
         exit(EXIT_FAILURE);
@@ -17,13 +17,16 @@ int main()
     {
         // Message was 'ping', wrappned inside a json
         // Valid
-        if (nativemsg_write_message((uint8_t*)"{\"msg\":\"pong\"}", 14) != 18)
+        if (nativemsg_write((uint8_t*)"{\"msg\":\"pong\"}", 14) != 18)
         {
             // Wrong number of bytes written - not (14 + 4)
             free(msg);
             exit(EXIT_FAILURE);
         }
     }
+    // Use `msg` for stuff here
+
+    // free `msg` when done
     free(msg);
     return 0;
 }
